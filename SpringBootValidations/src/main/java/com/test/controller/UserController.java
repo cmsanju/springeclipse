b264d475784c78a46d501f609252cfd27fadcc4f
sanjeevg <sanjeevg@192.168.0.4>
2021-05-12 11:40:21 +0530

    SpringBootValidationS with Swagger

---
+++ SpringBootValidations/src/main/java/com/test/controller/UserController.java
@@ -0,0 +1,50 @@
+package com.test.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.test.model.User;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+@Controller
+@Api(value = "UserController for validating the user fields")
+public class UserController {
+
+	@RequestMapping("/")
+	public String home() {
+
+		return "index";
+	}
+
+	@ApiOperation(value = "it will display user form and taking the user inputs")
+	@GetMapping("/register")
+	public String showForm(Model model, User user) {
+
+		model.addAttribute("user", user);
+
+		return "register_form";
+	}
+
+	@PostMapping("/register")
+	@ApiOperation(value = "validating and printing user details in success page")
+	public String submitForm(@Valid @ModelAttribute("user") User user ,BindingResult bindindResult, Model model ) {
+
+		if(bindindResult.hasErrors())
+		{
+
+			return "register_form";
+		}
+
+		return "register_success";
+	}
+}
