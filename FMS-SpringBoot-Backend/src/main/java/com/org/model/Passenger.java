cbeece2bdf479f740a95f10d9fbffe3473f258bc
sanjeevg <sanjeevg@192.168.0.5>
2021-05-30 21:01:23 +0530

    FMS Final app

---
+++ FMS-SpringBoot-Backend/src/main/java/com/org/model/Passenger.java
@@ -0,0 +1,61 @@
+package com.org.model;
+
+import java.math.BigInteger;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Passenger {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private BigInteger pnrNumber;
+	private String passengerName;
+	private int passengerAge;
+	private BigInteger passengerUIN;
+	private Double luggage;
+
+	public BigInteger getPnrNumber() {
+		return pnrNumber;
+	}
+
+	public void setPnrNumber(BigInteger pnrNumber) {
+		this.pnrNumber = pnrNumber;
+	}
+
+	public String getPassengerName() {
+		return passengerName;
+	}
+
+	public void setPassengerName(String passengerName) {
+		this.passengerName = passengerName;
+	}
+
+	public int getPassengerAge() {
+		return passengerAge;
+	}
+
+	public void setPassengerAge(int passengerAge) {
+		this.passengerAge = passengerAge;
+	}
+
+	public BigInteger getPassengerUIN() {
+		return passengerUIN;
+	}
+
+	public void setPassengerUIN(BigInteger passengerUIN) {
+		this.passengerUIN = passengerUIN;
+	}
+
+	public Double getLuggage() {
+		return luggage;
+	}
+
+	public void setLuggage(Double luggage) {
+		this.luggage = luggage;
+	}
+
+}
